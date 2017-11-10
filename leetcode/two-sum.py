0508f20546d701009420d0bdb1d0963bf3b17fd0
sergei <majd-kho>
2017-11-10 16:26:14 -0500

    first commit

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,7 +1,7 @@
-# Solution for two sum problem.
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# The solution makes use of an key-value pairs, and the properties of addition and division.
+#Solution for two sum problem.
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#The solution makes use of an key-value pairs, and the properties of addition and division.
 
 #! /usr/bin/python
 
