e936dc9bccb0416c2188175fe20478dae05a2598
majd-kho <kho.majds@gmail.com>
2017-11-12 22:14:26 -0500

    reverse-interger problem solution

--- leetcode/reverse-integer.py
+++ leetcode/reverse-integer.py
@@ -1,6 +1,6 @@
-# Solution for reverse integer problem.
-# Given a 32-bit signed integer, reverse digits of an integer.
-# Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+#Solution for reverse integer problem.
+#Given a 32-bit signed integer, reverse digits of an integer.
+#Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 class Solution(object):
     def reverse(self, x):
         maxNum = 2147483647;
