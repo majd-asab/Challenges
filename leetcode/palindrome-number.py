dd1b845b6c63c22b7c2c77fe97b9f43a470da28d
majd-kho <kho.majds@gmail.com>
2017-11-13 20:30:13 -0500

    added details

--- leetcode/palindrome-number.py
+++ leetcode/palindrome-number.py
@@ -1,3 +1,5 @@
+# Solution for Palindrome Problem
+# Determine whether an integer is a palindrome. Do this without extra space.
 class Solution(object):
     def isPalindrome(self, x):
         sign =  cmp(x,0);
