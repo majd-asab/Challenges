e149d1cd5727f0b91deae4f29deff53f2edecbe4
majd-kho <kho.majds@gmail.com>
2017-12-18 14:17:08 -0500

    binary addition solution

---
+++ leetcode/add-binary.py
@@ -0,0 +1,11 @@
+# add 2 binary strings together
+# return: STRING
+class Solution(object):
+    def addBinary(self, a, b):
+        return bin(int(a,2) + int(b,2))[2:]
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+
