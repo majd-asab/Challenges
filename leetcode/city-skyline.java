b20f6afa10eb19b73119debbd037b382ca8b8159
Majd <kho.majds@gmail.com>
2018-06-05 00:23:57 -0400

    prints out the new grid

--- leetcode/city-skyline.java
+++ leetcode/city-skyline.java
@@ -3,3 +3,43 @@ In a 2 dimensional array grid, each value grid[i][j] represents the height of a 
 At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, must be the same as the skyline of the original grid. A city's skyline is the outer contour of the rectangles formed by all the buildings when viewed from a distance. See the following example.
 What is the maximum total sum that the height of the buildings can be increased?
 */
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int[][] newGrid = new int[grid.length][grid[0].length];
+        
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0;j < grid[0].length; j++){
+               
+               int maxRow = maxInRow(grid[i]);
+               int maxColumn = maxInColumn(grid,j);
+               // if(maxRow < maxColumn){
+               //     newGrid[i][j] = maxRow;
+               // }else{
+               //     newGrid[i][j] = maxColumn;
+               // }
+               newGrid[i][j] = (maxRow < maxColumn)? maxRow : maxColumn;
+               System.out.print(newGrid[i][j]);
+            }
+             System.out.println();
+        }
+        return 0;
+    }
+    
+    // Return the max number in an array.
+    public int maxInRow(int[] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+    
+    // Return the max number out of a bunch of arrays at a specific index.
+    public int maxInColumn(int[][] grid, int index){
+        int max = grid[0][index];
+        for(int i = 0; i < grid.length; i++){
+            if(grid[i][index] > max) max = grid[i][index];
+        }
+        return max;
+    }
+}
